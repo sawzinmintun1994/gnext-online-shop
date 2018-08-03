d2a4b3723931edfea6c17cd84c06a1046ed4f3c5
mayzinoo <mayzinoognext@gmail.com>
2018-08-03 16:11:40 +0630

    Change header

--- src/components/header/index.vue
+++ src/components/header/index.vue
@@ -1,65 +1,68 @@
-<template>
-  <b-container class="header">
-      <b-row>
-          <b-col cols='12' sm='2' md='3' lg='6' xl='6'>Logo</b-col>
-          <b-col cols='12' sm='10' md='9' lg='6' xl='6' class="menu">
+<template id="">
+  <div class="header">
+  <b-container fluid >
+    <b-row cols="12">
+      <b-col cols="2" sm="2" md="3" lg="6">
+        Logo
+      </b-col>
+      <b-col class="menu" cols="10" sm="10" md="9" lg="6">
             <ul>
-              <router-link to="/"><li class="menu-home">Home</li></router-link>
-              <router-link to="/man"><li class="menu-man">Man</li></router-link>
-              <router-link to="/woman"><li class="menu-woman">Woman</li></router-link>
-              <router-link to="/kids"><li class="menu-kids">Kids</li></router-link>
-              <router-link to="/cart"><li class="menu-cart" v-if="order">Cart {{ count() }}</li></router-link>
-            </ul>
-          </b-col>
-      </b-row>
+             <router-link to="/"><li>Home</li></router-link>
+             <router-link to="/man"><li>Men</li></router-link>
+             <router-link to="/woman"><li>Women</li></router-link>
+             <router-link to="/kids"><li>Kids</li></router-link>
+           </ul>
+      </b-col>
+    </b-row>
   </b-container>
+</div>
 </template>
-
 <script>
 export default {
   data () {
     return {
-      order: localStorage.getItem('orderItems') || null
+      order: null || localStorage.getItem('orderitem'),
+      sta: null
     }
   },
   watch: {
     $route () {
-      this.order = localStorage.getItem('orderItems')
-    }
-  },
-  methods: {
-    count: function () {
-      return this.order
+      this.order = localStorage.orderitem
+      this.sta = localStorage.women
     }
   }
 }
 </script>
-
-<style lang="css">
-.header .row {
-  background: #f1f1f1;
-  padding: 20px;
-}
-.menu {
-
-}
-.menu ul {
-  margin: 0;
-  padding: 0;
-}
-.menu ul li {
-  list-style-type: none;
+<style scoped>
+.menu ul li{
   display: inline;
-  padding: 19px 20px;
-  cursor: pointer;
+  padding: 20px;
+  list-style-type: none;
+}
+.header {
+    margin-bottom: 55px;
+}
+.header .row{
+  padding: 15px;
+  background-color:Beige ;
+  position: fixed;
+  top: 0;
+  width: 100%;
+  z-index: 1;
+
 }
 .menu ul a {
   color: #000;
   text-decoration: none;
 }
-.menu ul a.router-link-exact-active {
-  border-bottom: 4px solid #000;
-  font-weight: bold;
-  padding-bottom: 20px;
+.menu ul {
+  margin: 0;
+  padding: 0;
 }
+a.router-link-exact-active{
+  border-bottom: 4px solid black;
+  font-weight: bold;
+  padding-bottom: 15px;
+}
+
 </style>
