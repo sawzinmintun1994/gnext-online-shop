a5a78658adb3b5817bbfb1dc27033fc3066a1f96
mayzinoo <mayzinoognext@gmail.com>
2018-08-06 09:05:26 +0630

    Head Change

--- src/components/header/index.vue
+++ src/components/header/index.vue
@@ -11,7 +11,75 @@
              <router-link to="/man"><li>Men</li></router-link>
              <router-link to="/woman"><li>Women</li></router-link>
              <router-link to="/kids"><li>Kids</li></router-link>
-           </ul>
+           </ul><template id="">
+             <div class="header">
+             <b-container fluid >
+               <b-row cols="12">
+                 <b-col cols="2" sm="2" md="3" lg="6">
+                   Logo
+                 </b-col>
+                 <b-col class="menu" cols="10" sm="10" md="9" lg="6">
+                       <ul>
+                        <router-link to="/"><li>Home</li></router-link>
+                        <router-link to="/man"><li>Men</li></router-link>
+                        <router-link to="/woman"><li>Women</li></router-link>
+                        <router-link to="/kids"><li>Kids</li></router-link>
+                      </ul>
+                 </b-col>
+               </b-row>
+             </b-container>
+           </div>
+           </template>
+           <script>
+           export default {
+             data () {
+               return {
+                 order: null || localStorage.getItem('orderitem'),
+                 sta: null
+               }
+             },
+             watch: {
+               $route () {
+                 this.order = localStorage.orderitem
+                 this.sta = localStorage.women
+               }
+             }
+           }
+           </script>
+           <style scoped>
+           .menu ul li{
+             display: inline;
+             padding: 20px;
+             list-style-type: none;
+           }
+           .header {
+               margin-bottom: 55px;
+           }
+           .header .row{
+             padding: 15px;
+             background-color:Beige ;
+             position: fixed;
+             top: 0;
+             width: 100%;
+             z-index: 1;
+
+           }
+           .menu ul a {
+             color: #000;
+             text-decoration: none;
+           }
+           .menu ul {
+             margin: 0;
+             padding: 0;
+           }
+           a.router-link-exact-active{
+             border-bottom: 4px solid black;
+             font-weight: bold;
+             padding-bottom: 15px;
+           }
+
+           </style>
+
       </b-col>
     </b-row>
   </b-container>
