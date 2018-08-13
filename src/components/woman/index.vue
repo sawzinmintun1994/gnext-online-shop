79ea2913ada2effc5311d97327f68d083e0b4c17
thida1496 <gnextthida@gmail.com>
2018-08-13 13:40:06 +0630

    Update woman Page

--- src/components/woman/index.vue
+++ src/components/woman/index.vue
@@ -17,12 +17,12 @@
                <div class="hoverimage">
                  <b-img :src="getImgUrl(result.image)" fluid alt="Responsive image" />
                  <div class="overlay">
-                   <h4 @click="detail()">View Detail</h4>
+                   <h4 @click="detail(index)" v-b-modal.modal1 class="viewDetail" >View Detail</h4>
                  </div>
                 </div>
                  <div class="card-body">
                    <h4>{{ result.name }}</h4>
-                   <div>{{ result.price }}</div>
+                   <div><span>$</span>{{ result.price }}</div>
                  </div>
                  <div class="text-block">
                    <h4>{{result.status}}</h4>
@@ -30,10 +30,22 @@
               </div>
          </b-col>
       </b-row>
+      <b-modal ref="myModal" hide-footer id="modal1" centered :title="modal.name">
+          <b-row>
+          <b-col>
+            <b-img  v-if="modal.image" :src="getImgUrl(modal.image)" fluid alt="Responsive image"/>
+          </b-col>
+          <b-col>
+            <h4>{{modal.status}}</h4>
+            <h4>${{modal.price}}</h4>
+             <input type="number" name="quantity" min='1' max='20' v-model="value"><br>
+             <b-btn type="button" class="btn" @click="Cart()">Add to Cart</b-btn>
+           </b-col>
+          </b-row>
+        </b-modal>
     </b-container>
   </div>
 </template>
-
 <script>
 export default {
   data () {
@@ -44,14 +56,29 @@ export default {
       price: '',
       status: '',
       select: null,
+      value: 1,
+      modal: {
+        image: null,
+        name: null,
+        status: null,
+        price: null
+      },
+      likeItem: {
+        title: '',
+        status: '',
+        price: 0,
+        picked: 'product',
+        quantity: '',
+        url: null
+      },
       womanArr: [
-        {Id: 1, name: 'Jacket', image: 'jacket.png', price: '$10', status: 'New Arrivals'},
-        {Id: 2, name: 'Jacket', image: 'jacket1.png', price: '$15', status: 'Best Seller'},
-        {Id: 3, name: 'Jacket', image: 'jacket3.png', price: '$71', status: 'New Arrivals'},
-        {Id: 4, name: 'Tank Top', image: 'tank.png', price: '$22', status: 'New Arrivals'},
-        {Id: 5, name: 'Tank Top', image: 'tank1.png', price: '$43', status: 'New Arrivals'},
-        {Id: 6, name: 'Tank Top', image: 'tank2.png', price: '$19', status: 'Best Seller'},
-        {Id: 7, name: 'Tank Top', image: 'tank3.png', price: '$29', status: 'Best Seller'}
+        {Id: 1, name: 'Jacket', image: 'jacket.png', price: 10, status: 'New Arrivals'},
+        {Id: 2, name: 'Jacket', image: 'jacket1.png', price: 11, status: 'Best Seller'},
+        {Id: 3, name: 'Jacket', image: 'jacket3.png', price: 33, status: 'New Arrivals'},
+        {Id: 4, name: 'Tank Top', image: 'tank.png', price: 15, status: 'New Arrivals'},
+        {Id: 5, name: 'Tank Top', image: 'tank1.png', price: 20, status: 'New Arrivals'},
+        {Id: 6, name: 'Tank Top', image: 'tank2.png', price: 30, status: 'Best Seller'},
+        {Id: 7, name: 'Tank Top', image: 'tank3.png', price: 25, status: 'Best Seller'}
       ]
     }
   },
@@ -113,8 +140,38 @@ export default {
         this.result = val
       }
     },
-    detail () {
-
+    detail (index) {
+      this.modal.name = this.result[index].name
+      this.modal.status = this.result[index].status
+      this.modal.image = this.result[index].image
+      this.modal.price = this.result[index].price
+      this.likeItem.url = this.modal.image
+    },
+    Cart () {
+      // this.likeItem.picked = this.picked
+      // console.log(this.likeItem.picked)
+      this.likeItem.title = this.modal.name
+      this.likeItem.status = this.modal.status
+      this.likeItem.price = this.modal.price
+      this.likeItem.quantity = this.value
+      if (typeof (Storage) !== 'undefined') {
+        var orderLists = []
+        var checkItems = JSON.parse(localStorage.getItem('cartItem'))
+        if (typeof checkItems === 'undefined' || checkItems === null) {
+          orderLists[0] = this.likeItem
+          localStorage.setItem('cartItem', JSON.stringify(orderLists))
+          checkItems = 0
+        } else {
+          checkItems[checkItems.length] = this.likeItem
+          // localStorage.removeItem('cartItem')
+          localStorage.setItem('cartItem', JSON.stringify(checkItems))
+          checkItems = checkItems.length
+        }
+        this.$router.push({ path: '/woman?' + checkItems++ })
+      } else {
+        console.log('no support')
+      }
+      this.$refs.myModal.hide()
     }
   }
 }
@@ -160,6 +217,9 @@ export default {
         font-size: 20px;
         padding: 10px;
         text-align: center;
+        .viewDetail{
+          cursor: pointer;
+        }
       }
       .hoverimage:hover .overlay {
           opacity: 1;
@@ -182,6 +242,9 @@ export default {
         font-family: sans-serif;
       }
     }
+  }
+  .btn{
+    margin-top: 10px;
   }
 }
 </style>
